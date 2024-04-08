eb5b63e08f08afd597df032275f8ca75d5168cbb
Данііл Плотніков <pdaniil116@gmail.com>
2024-04-08 19:27:32 +0300

    Add new

---
+++ Lesson_2/Number_Pl/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double price = 100; 
+        double discountPercentage = 20; 
+
+        double discountAmount = price * (discountPercentage / 100);
+
+        Console.WriteLine("Price of the product: $" + price);
+        Console.WriteLine("Discount percentage: " + discountPercentage + "%");
+        Console.WriteLine("Amount of the discount: $" + discountAmount);
+
+        Console.ReadKey();
+    }
+}

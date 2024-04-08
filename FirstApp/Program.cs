4013bba007812ca02c311227f0c1845d9c3b7e90
Данііл Плотніков <pdaniil116@gmail.com>
2024-04-08 19:07:24 +0300

    Add new project

---
+++ FirstApp/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace HelloWorld
+
+{
+
+    class Program
+
+    {
+
+        static void Main(string[] args)
+
+        {
+
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+
+            Console.WriteLine("Привіт, я навчаюся у школі Hillel!");
+
+            Console.ReadKey();
+
+        }
+
+    }
+
+}
